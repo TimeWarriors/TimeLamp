6c5858e629f17e6229501598d2018ab191e7197a
Anton <a223jw@student.lnu.se>
2016-02-01 14:41:43 +0100

    Started with the setings code added a JSON file wich holds the names, ids and rooms of lamps. Next step will be connecting them with the settings.js file

--- settings/README.md
+++ settings/README.md
@@ -1,1 +1,17 @@
-settings
+settings.json - Innehåller alla lampor som objekt och kan få in en ny lampa när som helst.
+                För att lägga till en ny lampa skapa ett objekt i arrayn.
+                Exempel:
+                        [
+                            {
+                                "type": "blink",
+                                "id": "Lägg till id på lampan"
+                            },
+
+                            {
+                                "type": "hue",
+                                "id": "Lägg till id på lampan",
+                                "room": "Lägg till id på rum"
+                            }
+                        ]
+
+settings.js -
--- settings/settings.js
+++ settings/settings.js
@@ -1,1 +1,44 @@
 "use strict";
+
+// Här sköts inställningarna på lamporna. Vi kommer ha inställningar för både Blink(1) mk 2
+// och för Philips hue lampan. Vi har en JSON fil som kommer innehålla information om TYP, ID, mm.
+
+//en fil (settings.json)
+// DETTA ÄR TYP EN DAL KLASS för settings. Från denna klass ska man kunna hämta alla objekt beroende
+// på typ, id, mm.
+
+const Settings = class {
+
+    var fsp = require('fs-promise');
+    var name;
+    var id;
+
+    //JSONParser parser = new JSONParser();
+    //Object settingsJSON = parser.parse("settings.json");
+    //JSONObject jsonObject = (JSONObject) settingsJSON;
+
+    constructor() {
+    }
+
+    getName(){
+        fsp.readFile("settings.json", {encoding;'utf8'}).then(function(data){
+            //name = (String) jsonObject.get("Name");
+            if("" = "hue")
+            {
+                name = "hue";
+            }
+            name = "blink";
+        }).catch(function(error){
+            console.log("error the Getname dont work.")
+        }
+    }
+
+    getID(){
+        fsp.readFile("settings.json", {encoding;'utf8'}).then(function(data){
+            if("" = id)
+            {
+                console.log("hej");
+            }
+        };
+    }
+}
