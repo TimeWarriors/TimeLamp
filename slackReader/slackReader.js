4a2576740ad4b03a347446206f4f1062add107f6
Lucas <lucas.94wik@gmail.com>
2016-02-03 15:27:16 +0100

    Can read the latest message from leitets feed, but right now we found another way that would simplify the whole procoess and we wouldent really have to write anything. You can connect IFTTT directly to a blink(1) lamp and then we wouldent need the use of the slack API nor create a RESTAPI, only thing is that we wouldent really do any of that work at all.

--- slackReader/slackReader.js
+++ slackReader/slackReader.js
@@ -1,32 +1,90 @@
 "use strict"
+
+let http = require('http');
+let fsp = require('fs-promise');
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+let swedishIsAtLocation = "Jag kom till";
+let englishIsAtLocation = "I entered";
+
+
+
+
 let slackReader = function(){
 
 }
 
-slackReader.prototype.getLecutrerStatus = function(){
-	
-	//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    
-	
-	//var xhr1 = new XMLHttpRequest();
-	//authenticate
-	//Add
-	var settings = getSlackSettings();
-	var params = "client_id="+getSlackSettings().clientID+"&scope=" + getSlackSettings().scope;	
-	window.location = "https://slack.com/oauth/authorize?" + params; ///
-	//xhr1.open("GET", "http://slack.com/oauth/authorize/")
+ 
 
-    var xhr = new XMLHttpRequest();
-    //xhr.open("GET", "https://slack.com/api/groups.history");
-	xhr.open("GET", "https://slack.com/oauth/authorize?" + params);
+let getLecutrerStatus = function(channelID, callback){
 	
-    xhr.addEventListener("load", function(){
-        console.log(xhr.responseText);
-        statusWindow.innerHTML = xhr.responseText;
-		console.log(xhr.getResponseHeader);
-    })
+	
+	var options = {
+		host:null,
+		path:null,
+		method:null
+	};
 
-    xhr.send();
+	fsp.readFile('slackReaderSettings.json', {encoding : 'utf8'}).then((contents) =>{
+		
+		let parsedContent = JSON.parse(contents);				
+	
+		var options = {
+			host:parsedContent.host,
+			path:parsedContent.scope + "token="+ parsedContent.token +"&channel=C053AE0SB&count=1",
+			method:'GET'
+		};
+		
+		console.log(options.host)
+		
+		let xhr = new XMLHttpRequest();
+		
+		xhr.open("GET", parsedContent.host+parsedContent.scope + "token=" + parsedContent.token + "&channel=C053AE0SB&count=3");
+	
+		xhr.addEventListener("load", function(){
+			var lastUpdate = JSON.parse(xhr.responseText).messages[0].attachments[0].text;
+
+			if(lastUpdate.indexOf(swedishIsAtLocation) > -1 || lastUpdate.indexOf(englishIsAtLocation) > -1){
+				console.log("Personen är på platsen")
+				//callback(true);
+			}
+			else{
+				console.log("personen är inte på platsen")
+				//callback(false);
+			}
+		})
+
+		xhr.send();
+
+		/*let req = http.get(options, function(res){
+			console.log("Inne i get")
+			console.log(res.statusCode);
+			console.log(JSON.stringify(res.headers));
+			
+			res.setEncoding('application/json');
+			res.on('data', function(chunk){
+				console.log("BODY: " + chunk);
+			})
+		
+		})
+		
+		
+		
+		req.on('error', (e) =>{
+			console.log('Error with request '+e.message)
+		})
+		
+		req.on('end', () =>{
+			console.log("all data collected")
+		})*/
+	});
+	
+		
+	console.log("efter get")
 }
+
+getLecutrerStatus();
+
+
 
 module.exports = slackReader;
