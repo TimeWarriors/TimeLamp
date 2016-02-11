038250fb5448c3b1477dff8e821f3fcf8dd82e40
jt222ii <jt222ii@student.lnu.se>
2016-02-11 15:52:10 +0100

    after a ton of discussion and thinking how we should the color between two colors I have created a function that uses hue instead of rub as hue is a color native to philips hue and does not need a conversion. Seems to work fine right now.

--- lightHandler/test.js
+++ lightHandler/test.js
@@ -3,17 +3,27 @@ let LightHandler = require('./lightHandler.js');
 let lightHandler = new LightHandler();
 
 ////ändra till grön
-//lightHandler.changeColor("4", 0, 255, 0, 10);
-//lightHandler.changeColor("3", 255, 0, 0, 0);
+
+lightHandler.changeColorWithHue("4", 0, 1);
+lightHandler.changeColorWithHue("3", 0, 1);
+
+//lightHandler.changeColorWithHue("4", 46920, 1);
+//lightHandler.changeColorWithHue("3", 56228, 1);
+
+//lightHandler.changeColorWithHue("4", 65535, 60);
+//setTimeout(function(){lightHandler.changeColorWithHue("3", 65535, 30)}, 30000)
+
+//lightHandler.changeColor("4", 0, 255, 0, 0);
+//lightHandler.changeColor("3", 255, 255, 0, 0);
 
 //lightHandler.setWarning("4", 1000, 10);
 //lightHandler.setWarning("3", 200, 5);
 
-//lightHandler.changeColor("4", 0, 255, 0, 5);
-//lightHandler.changeColor("3", 0, 255, 0, 5);
+//lightHandler.changeColor("4", 255, 0, 0, 60);
+//setTimeout(function(){lightHandler.changeColor("3", 255, 0, 0, 30)}, 30000)
 
 //lightHandler.On("4", true);
 //lightHandler.On("4", false);
 
-//lightHandler.changeBrightness("3", 100, 1);
-//lightHandler.changeBrightness("4", 100, 1);
+//lightHandler.changeBrightness("3", 255, 1);
+//lightHandler.changeBrightness("4", 255, 1);
