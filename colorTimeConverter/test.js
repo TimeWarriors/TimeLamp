c58d2f33dfa4dba6ca8eb58fba56e11641a631c4
jt222ii <jt222ii@student.lnu.se>
2016-02-12 14:55:32 +0100

    started with making a function that returns the correct hue color

---
+++ colorTimeConverter/test.js
@@ -0,0 +1,5 @@
+'use strict'
+let ColorTimeConverter = require('./colorTimeConverter.js');
+let colorTimeConverter = new ColorTimeConverter();
+
+colorTimeConverter.getColor(120, 60, 90, 25500, 20678);
