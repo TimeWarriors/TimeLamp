1b46ae531d8fab752506317a6b332796343f6958
jt222ii <jt222ii@student.lnu.se>
2016-02-12 15:40:45 +0100

    tested getColor. Seems to work.

--- colorTimeConverter/test.js
+++ colorTimeConverter/test.js
@@ -2,4 +2,4 @@
 let ColorTimeConverter = require('./colorTimeConverter.js');
 let colorTimeConverter = new ColorTimeConverter();
 
-colorTimeConverter.getColor(120, 60, 90, 25500, 20678);
+colorTimeConverter.getColor(10, 0, 3, 20678, 0);
