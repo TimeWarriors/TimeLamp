e563382c849ae37dd97878e974fb216a8f4e58cd
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2016-02-15 12:47:02 +0100

    change some code in socket.io

--- IFTTT_restAPI/client_prototype/js/socket.js
+++ IFTTT_restAPI/client_prototype/js/socket.js
@@ -1,33 +1,15 @@
 var socket = io();
 
-
-
-socket.on('statusUpdated', function(userData){
+    socket.on('statusUpdated', function(userData){
 	console.log(userData);
     console.log("hejsan2");
-    
     var teachRes = document.getElementById("teachRes");
     
     if(userData.presence == true){
-                
-                teachRes.style.backgroundColor = "#ADFF2F";
-                // teachDiv.style.boxShadow = "10px 0 120px seagreen";
-                // teachDiv.style.boxShadow = "10px 10px 250px seagreen"; 
-                
+        teachRes.style.backgroundColor = "#ADFF2F";
             }
             else{
                 teachRes.style.backgroundColor = "#B22222";
-                
             }
     
 })
-
-// socket.on('statusUpdated', function(userData){
-//   socket.on('event', function(userData){
-//       console.log("hejsaaa");
-//   }
-  
-//   );
-//   socket.on('disconnect', function(){});
-// });
-// server.listen(3000);
