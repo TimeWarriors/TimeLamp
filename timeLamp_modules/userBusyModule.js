8609da1002418a59d4fdd748d964d535283de195
Lucas <lucas.94wik@gmail.com>
2016-02-16 13:12:45 +0100

    This branch will most likely not be used any more, just tested to make a simple module but will try to do it's functionallity directily in the IFTTT rest API.

--- timeLamp_modules/userBusyModule.js
+++ timeLamp_modules/userBusyModule.js
@@ -2,11 +2,11 @@ var timeEditDAL = require('../timeeditDAL/timeeditDAL.js')
 
 function userBusyModule(){
 	this.timeEditApiLnu = new timeEditDAL(		
-                'https://se.timeedit.net/web/lnu/db1/schema1/',
+                "https://se.timeedit.net/web/lnu/db1/schema1/",
                 3
             );
 	
-	console.log(this.timeEditApiLnu);
+	//console.log(this.timeEditApiLnu);
 }
 
 exports.run = userBusyModule();
