7807daddc12fd7bcc8ba3aa44debc66030db4f66
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2016-02-16 15:11:16 +0100

    save sync

--- IFTTT_restAPI/server.js
+++ IFTTT_restAPI/server.js
@@ -79,9 +79,3 @@ app.post('/update/:id/:presence', function(req, res){
 http.listen(3000, function(){
 	console.log("listening on 3k");
 });
-
-test.createServer(function(req, res){
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.write("test av server");
-    res.end('It works');
-}).listen(3000, '194.47.106.229');
