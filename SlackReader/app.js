9be17d66043ed54796db74bfa0c18e4810f0415e
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-02-18 11:31:49 +0100

    SlackReader folder added

---
+++ SlackReader/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const SlackReader = require('./SlackAPI/SlackReader.js');
+
+
+var slack = new SlackReader();
+
+slack.getAllChannels()
+    .then(channelsObject => {
+        // Test purpose. Get this array from somewhere else.
+        var channelNames = ['1dv409', '1dv405', '1dv450'];
+        return slack.getChannelNamesAndIDs(channelsObject, channelNames);
+    })
+    .then(channels => {
+        // Get 'channelName' from TimeEdit.
+        var channelName = '1dv450';
+        return slack.getChannelID(channels, channelName);
+    })
+    .then(channelID => {
+        // Test purpose. Get starttime from TimeEdit.
+        var lectureStartTime = '1455106500'; // 10/2-2016 Kl: 13:15.
+        return slack.getChatMessagesFromStartTime(channelID, lectureStartTime);
+    })
+    .then(messages => {
+        // Test purpose.
+        var postedMessages = 0;
+
+        if (slack.isNewMessagePosted(messages, postedMessages)) {
+            var newMessages = slack.getNewMessages(messages, postedMessages);
+            return slack.searchForHashTags(newMessages);
+        } else {
+            console.log('No new messages!');
+        }
+    })
+    .then(messages => {
+        console.log(messages);
+        // TODO: Call functions depending on hashtags.
+    })
+    .catch(error => {
+        console.log(error);
+    });
