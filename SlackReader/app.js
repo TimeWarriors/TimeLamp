aaaac6b15ac1e5f28031992a3f9c977d61a64e2f
Wictor Kihlbaum <wk222as@student.lnu.se>
2016-03-01 16:15:25 +0100

    WebSocket functionality implemented

--- SlackReader/app.js
+++ SlackReader/app.js
@@ -5,9 +5,10 @@ let slackAPI = new SlackAPI();
 
 
 
-// Update ConfigFile for Channels.
+/* Update ConfigFile for Channels */
 //slackAPI.updateChannelsFile();
 
-// Get latest messages for specific Slack channel.
-// Parameters: course-code, starttime in millisec.
-slackAPI.getMessages('1dv450', '1455106500');
+/* Get latest messages for specific Slack channel */
+//slackAPI.getMessages('1dv450', '1455106500');
+
+slackAPI.initWebSocket();
